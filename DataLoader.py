4aad15346eba2993729fbd4a6b4a0ef67a6d21c9
ZhikunWei <1045525044@qq.com>
2019-12-07 21:23:49 +0800

    Initial commit

---
+++ DataLoader.py
@@ -0,0 +1,29 @@
+import os
+from torchvision import datasets, models, transforms
+from distributed.protocol import torch
+
+class MyDataLoader:
+    def __init__(self, data_dir, input_size, batch_size):
+        self.data_dir = data_dir
+        self.input_size = input_size
+        self.batch_size = batch_size
+        self.data_transforms = {
+            'train': transforms.Compose([
+                transforms.RandomResizedCrop(self.input_size),
+                transforms.RandomHorizontalFlip(),
+                transforms.ToTensor(),
+                transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+            ]),
+            'val': transforms.Compose([
+                transforms.Resize(self.input_size),
+                transforms.CenterCrop(self.input_size),
+                transforms.ToTensor(),
+                transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+            ]),
+        }
+
+image_datasets = {x: datasets.ImageFolder(os.path.join(data_dir, x), data_transforms[x]) for x in ['train', 'val']}
+# Create training and validation dataloaders
+dataloaders_dict = {
+    x: torch.utils.data.DataLoader(image_datasets[x], batch_size=batch_size, shuffle=True, num_workers=4) for x in
+    ['train', 'val']}
---
+++ data_preprocess.py
@@ -0,0 +1,67 @@
+import cv2
+import os
+
+def unpickle(file):
+    import pickle
+    with open(file, 'rb') as fo:
+        d = pickle.load(fo, encoding='bytes')
+    return d
+
+
+def restore_images_cifar10_train():
+    for i in range(1, 6):
+        batch= unpickle('data/cifar10/data_batch_'+str(i))
+        print(batch)
+        image_arrays = batch[b'data']
+        labels = batch[b'labels']
+        filenames = batch[b'filenames']
+        for index in range(len(labels)):
+            image_array = image_arrays[index]
+            red = image_array[:1024]
+            green = image_array[1024:2048]
+            blue = image_array[2048:]
+            red_image = red.reshape(32, 32)
+            green_image = green.reshape(32, 32)
+            blue_image = blue.reshape(32, 32)
+            merged = cv2.merge([blue_image, green_image, red_image])
+
+            label_name = label_names[labels[index]].decode()
+            filename = filenames[index].decode()
+            if not os.path.exists(os.path.join('data/cifar10/train/', label_name)):
+                os.mkdir(os.path.join('data/cifar10/train/', label_name))
+            cv2.imwrite(os.path.join('data/cifar10/train/', label_name, filename), merged)
+
+
+def restore_image_cifar10_val():
+    batch = unpickle('data/cifar10/test_batch')
+    image_arrays = batch[b'data']
+    labels = batch[b'labels']
+    filenames = batch[b'filenames']
+    for index in range(len(labels)):
+        image_array = image_arrays[index]
+        red = image_array[:1024]
+        green = image_array[1024:2048]
+        blue = image_array[2048:]
+        red_image = red.reshape(32, 32)
+        green_image = green.reshape(32, 32)
+        blue_image = blue.reshape(32, 32)
+        merged = cv2.merge([blue_image, green_image, red_image])
+
+        label_name = label_names[labels[index]].decode()
+        filename = filenames[index].decode()
+        if not os.path.exists(os.path.join('data/cifar10/val/', label_name)):
+            os.mkdir(os.path.join('data/cifar10/val/', label_name))
+        cv2.imwrite(os.path.join('data/cifar10/val/', label_name, filename), merged)
+
+
+def check_label_names():
+    d = unpickle('data/cifar10/batches.meta')
+    label_names = d[b'label_names']
+    return label_names
+
+
+label_names = check_label_names()
+
+
+if __name__ == '__main__':
+    restore_images_cifar10_train()
